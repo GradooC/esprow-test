ebbe2af96a42255a2c477921bb3e11f5ab8aaadd
Щемелёв Евгений Владимирович <ESchemelev@alfabank.ru>
2022-04-14 22:06:10 +0300

    wrap the Textarea component with forwardRef HOC

--- src/Common/textarea/textarea.tsx
+++ src/Common/textarea/textarea.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
 
 type TextareaProps = {
 	label?: string;
 	defaultValue: string;
 };
 
-export function Textarea({ label, defaultValue }: TextareaProps) {
-	return (
-		<div>
-			<label htmlFor={`textarea-${label}`}>
-				<p className="inline-block uppercase text-gray-700 text-xs font-bold mb-1">
-					{label}
-				</p>
-				<textarea
-					id={`textarea-${label}`}
-					className="w-full bg-gray-200 text-gray-700 border border-emerald-500 rounded py-2 px-4 focus:outline-none focus:bg-white"
-					defaultValue={defaultValue}
-				/>
-			</label>
-		</div>
-	);
-}
+export const Textarea = forwardRef<HTMLTextAreaElement, TextareaProps>(
+	({ label, defaultValue }, ref) => {
+		return (
+			<div>
+				<label htmlFor={`textarea-${label}`}>
+					<p className="inline-block uppercase text-gray-700 text-xs font-bold mb-1">
+						{label}
+					</p>
+					<textarea
+						ref={ref}
+						id={`textarea-${label}`}
+						className="w-full bg-gray-200 text-gray-700 border border-emerald-500 rounded py-2 px-4 focus:outline-none focus:bg-white"
+						defaultValue={defaultValue}
+					/>
+				</label>
+			</div>
+		);
+	}
+);
 
 Textarea.defaultProps = {
 	label: 'textarea',
