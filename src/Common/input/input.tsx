d5ccdd1f630b082b3ab2eeb17db7a40b2d003856
Щемелёв Евгений Владимирович <ESchemelev@alfabank.ru>
2022-04-14 22:05:58 +0300

    wrap the Input component with forwardRef HOC

--- src/Common/input/input.tsx
+++ src/Common/input/input.tsx
@@ -1,24 +1,27 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
 import { InputType } from './types';
 
 type InputProps = InputType & {
 	label: string;
 };
 
-export function Input({ type, label, defaultValue }: InputProps) {
-	return (
-		<div>
-			<label htmlFor={`input-${label}`}>
-				<p className="inline-block uppercase text-gray-700 text-xs font-bold mb-1">
-					{label}
-				</p>
-				<input
-					className="w-full bg-gray-200 text-gray-700 border border-emerald-500 rounded py-2 px-4 focus:outline-none focus:bg-white"
-					id={`input-${label}`}
-					type={type}
-					defaultValue={defaultValue}
-				/>
-			</label>
-		</div>
-	);
-}
+export const Input = forwardRef<HTMLInputElement, InputProps>(
+	({ type, label, defaultValue }, ref) => {
+		return (
+			<div>
+				<label htmlFor={`input-${label}`}>
+					<p className="inline-block uppercase text-gray-700 text-xs font-bold mb-1">
+						{label}
+					</p>
+					<input
+						ref={ref}
+						className="w-full bg-gray-200 text-gray-700 border border-emerald-500 rounded py-2 px-4 focus:outline-none focus:bg-white"
+						id={`input-${label}`}
+						type={type}
+						defaultValue={defaultValue}
+					/>
+				</label>
+			</div>
+		);
+	}
+);
