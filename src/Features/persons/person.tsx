ae0d8656ed44634071961131e3f489c75eb923bb
Щемелёв Евгений Владимирович <ESchemelev@alfabank.ru>
2022-04-15 18:52:56 +0300

    improve performance of the Person component

--- src/Features/persons/person.tsx
+++ src/Features/persons/person.tsx
@@ -1,58 +1,59 @@
-import React from 'react';
+import React, { memo } from 'react';
 import { type Person as PersonType } from '@Types/person';
 import { ReactComponent as EditIcon } from 'Icons/edit.svg';
 import { IconButton } from 'Common/icon-button';
 
 type PersonProps = PersonType & { onEdit: (id: string) => void };
 
-export function Person({
-	id,
-	isActive,
-	picture,
-	age,
-	name,
-	email,
-	address,
-	about,
-	registered,
-	onEdit,
-}: PersonProps) {
-	const handlePersonClick = () => {
-		onEdit(id);
-	};
+export const Person = memo(
+	({
+		id,
+		isActive,
+		age,
+		name,
+		email,
+		address,
+		about,
+		registered,
+		onEdit,
+	}: PersonProps) => {
+		const handlePersonClick = () => {
+			onEdit(id);
+		};
 
-	return (
-		<li className="rounded-md bg-white p-5">
-			<div className="flex items-center justify-between">
-				<h4 className="text-2xl">{name}</h4>
-				<div
-					className={`${
-						isActive ? 'bg-green-400' : 'bg-red-400'
-					} ml-3 w-4 h-4 rounded-full`}
-				/>
-				<p className="text-sm text-gray-400 mr-auto ml-3">
-					{new Date(registered).toDateString()}
+		return (
+			<li className="rounded-md bg-white p-5">
+				<div className="flex items-center justify-between">
+					<h4 className="text-2xl">{name}</h4>
+					<div
+						className={`${
+							isActive ? 'bg-green-400' : 'bg-red-400'
+						} ml-3 w-4 h-4 rounded-full`}
+					/>
+					<p className="text-sm text-gray-400 mr-auto ml-3">
+						{new Date(registered).toDateString()}
+					</p>
+					<IconButton onClick={handlePersonClick}>
+						<EditIcon width={30} />
+					</IconButton>
+				</div>
+				<p>
+					<strong>Age:&nbsp;</strong>
+					{age}
 				</p>
-				<IconButton onClick={handlePersonClick}>
-					<EditIcon width={30} />
-				</IconButton>
-			</div>
-			<p>
-				<strong>Age:&nbsp;</strong>
-				{age}
-			</p>
-			<p>
-				<strong>Email:&nbsp;</strong>
-				{email}
-			</p>
-			<p>
-				<strong>Address:&nbsp;</strong>
-				{address}
-			</p>
-			<div className="flex">
-				<strong>About:&nbsp;</strong>
-				<p>{about}</p>
-			</div>
-		</li>
-	);
-}
+				<p>
+					<strong>Email:&nbsp;</strong>
+					{email}
+				</p>
+				<p>
+					<strong>Address:&nbsp;</strong>
+					{address}
+				</p>
+				<div className="flex">
+					<strong>About:&nbsp;</strong>
+					<p>{about}</p>
+				</div>
+			</li>
+		);
+	}
+);
--- src/Features/persons/persons.tsx
+++ src/Features/persons/persons.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { useAppDispatch, useAppSelector } from 'App/hooks';
 import { startEditing } from 'Features/edit-person';
 import { Person } from './person';
@@ -8,12 +8,12 @@ export function Persons() {
 	const dispatch = useAppDispatch();
 	const persons = useAppSelector(selectAllPersons);
 
-	const handleEditPerson = (id: string) => {
+	const handleEditPerson = useCallback((id: string) => {
 		dispatch(startEditing(id));
-	};
+	}, [dispatch]);
 
 	return (
-		<ul className="flex flex-col space-y-5 bg-slate-400 px-10">
+		<ul className="flex flex-col space-y-5 bg-slate-400 px-10 py-4">
 			{persons.map(person => (
 				<Person key={person.id} {...person} onEdit={handleEditPerson} />
 			))}
