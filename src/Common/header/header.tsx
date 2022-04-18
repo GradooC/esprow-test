9d86e96aa8e5b0ef9f954d8012286fdfb4e4fff6
Щемелёв Евгений Владимирович <ESchemelev@alfabank.ru>
2022-04-18 10:07:50 +0300

    add some styles to Header

--- src/Common/header/header.tsx
+++ src/Common/header/header.tsx
@@ -6,7 +6,7 @@ type HeaderProps = {
 
 export function Header({ children }: HeaderProps) {
 	return (
-		<header className="fixed inset-x-0 top-0 flex justify-center bg-sky-700/60 backdrop-blur-sm">
+		<header className="fixed inset-x-0 top-0 flex justify-center bg-sky-700/60 backdrop-blur-sm shadow-md z-10">
 			<div className="flex flex-start max-w-4xl min-w-min w-full p-5">{children}</div>
 		</header>
 	);
